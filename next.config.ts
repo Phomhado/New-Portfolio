f592871457b4b9285bc6638325c02bd56ee87bca
Pedro Henrique de Oliveira <pedro.he.oli10@gmail.com>
2025-01-29 00:20:35 -0300

    fix: error on build next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.GITHUB_PAGES_BASE_PATH || "", 
+  trailingSlash: true, 
 };
 
 export default nextConfig;
